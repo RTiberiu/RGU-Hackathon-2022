64417beb61d22ab6b26ffa574dd74cc24edca670
RTiberiu <72140679+RTiberiu@users.noreply.github.com>
2022-03-27 05:22:10 +0100

    Fixed box array

--- js/javascript.js
+++ js/javascript.js
@@ -3,10 +3,10 @@ import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples
 $(document).ready(function() {
     
     // Game board
-    let x = 10;
-    let y = 10;
-    let z = 7;
-    let gameBoard = new Array(x).fill(new Array(y).fill(new Array(z)));
+    const xLimit = 10;
+    const yLimit = 10;
+    const zLimit = 7;
+    let gameBoard = new Array(xLimit).fill(new Array(yLimit).fill(new Array(zLimit)));
         
     // Ships points  
     let ships = [
@@ -17,7 +17,128 @@ $(document).ready(function() {
         [[1, 9, 5], [1, 8, 5]]
     ];
 
-    let boxes = [[]];
+    var checkedPoints = [];
+    let shipBoxes = [];
+    let otherBoxes = [];
+
+    // Players
+    var player1 = {
+        score: 0,
+        name: "James",
+        choice: [null, null, null]
+    }
+
+    var player2 = {
+        score: 0,
+        name: "Olaf",
+        choice: [null, null, null]
+    }
+    var currentPlayer = player1;
+
+    function submitChoice() {
+        let xChoice = $('#xCor').val();
+        let yChoice = $('#yCor').val();
+        let zChoice = $('#depthCor').val();
+        if (
+            (xChoice >= 0 && xChoice <= xLimit)
+            && (yChoice >= 0 && yChoice <= yLimit)
+            && (zChoice >= 0 && zChoice <= zLimit)
+        ) {
+            currentPlayer.choice = [xChoice, yChoice, zChoice];
+            checkCoordinates(currentPlayer);
+            return false;
+        } else {
+            alert("Provided input is outside specified boundaries!");
+        }
+    }
+
+    $("#theForm").submit(function (e) {
+        e.preventDefault(); // <==stop page refresh==>
+        submitChoice();
+    });
+
+    function updateScore() {
+        $('#player1Score').text(player1.score);
+        $('#player2Score').text(player2.score);
+        $('#whosMoving').text("Moving now: " + currentPlayer.name);
+    }
+
+    function compareCoordinates(co1, co2) {
+        if (co1[0] == co2[0]
+            && co1[1] == co2[1]
+            && co1[2] == co2[2]
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function checkCoordinates(player) {
+        console.log("Player's choice: " + JSON.stringify(player.choice));
+        if (checkCoordinatessHelper(player.choice, false)) {
+            alert('Congratulations, you hit a ship');
+            // Spawn box for hit
+            addBox(player.choice, true, false);
+            player.score += 10;
+            updateScore();
+        } else {
+            // Spawn box for miss
+            addBox(player.choice, false, false);
+
+            if (currentPlayer == player1) {
+                currentPlayer = player2;
+            } else if (currentPlayer == player2) {
+                currentPlayer = player1;
+            }
+            updateScore();
+            alert('You missed, ' + currentPlayer.name + ' is next');
+        }
+    } // end of outer Check coordinates function
+
+    function checkCoordinatessHelper(coordinates, returnShip) {
+        let shipHit = false;
+        let shipIndex = null;
+        let theWholeShip = null;
+
+        if (checkedPoints.length > 0) {
+            for (let i = 0; i < checkedPoints.length; i++) {
+                if (compareCoordinates(checkedPoints[i], coordinates)) {
+                    console.log("Double!!! " + checkedPoints[i]);
+                    return false;
+                }
+            }
+        }
+
+        checkedPoints.push(coordinates);
+        for (var ship = 0; ship < ships.length; ship++) {
+            for (let i = 0; i < ships[ship].length; i++) {
+                let co = ships[ship][i];
+                if (compareCoordinates(coordinates, co)) {
+                    console.log("Jackpot!");
+                    shipHit = true;
+                    shipIndex = ship;
+                    theWholeShip = ships[ship];
+                }
+            } // end of level 3 for loop
+        }
+
+        if (!shipHit) { // ship not hit
+            return false;
+        } else {
+            if (returnShip) { // ship hit, returnship == true
+                // Check if ship if completed
+
+                
+                let output = [shipIndex, theWholeShip];
+                // console.log(output);
+                return output;
+
+            } else { // ship hit, returnship == false
+                return true;
+            }
+        }
+    } // end of helper function
 
     // ----- Three JS ----- 
     // Setup scene and camera
@@ -60,11 +181,11 @@ $(document).ready(function() {
     scene.add(gridHelperIII);
 
     // Set camera positions
-    camera.position.set(20, -40, 20); // 0, -20, 40
+    camera.position.set(20, -40, 20); 
     controls.update();
 
     // Add box to plane
-    function addBox(coordinates) {
+    function addBox(coordinates, hit, completed, shipIndex) {
         // Create the box and its lines
         let boxGeo = new THREE.BoxGeometry(1, 1, 1);
         const edges = new THREE.EdgesGeometry(boxGeo);
@@ -72,25 +193,27 @@ $(document).ready(function() {
         
         const box = new THREE.Mesh(boxGeo);
         
-        // Push box into array 
-        boxes[0].push(box);
-
-        // Choose box colors depending on hit, miss, or completed
-        let boxMaterial;
-        console.log(coordinates);
-        if (coordinates[4] == true) {
+        // Push box into array
+        if (hit) {
+            if (shipBoxes[shipIndex] == undefined) {
+                shipBoxes.push([]);
+                shipBoxes[shipIndex].push(box);
+            } else {
+                shipBoxes[shipIndex].push(box);
+            }
+        } else {
+            // If not part of ship, insert in generic boxes array
+            otherBoxes.push(box);
+        }
+        
+        // Choose box colors depending on hit, miss, or completed        
+        if (completed != false) {
             // If completed
-
-            // Test arr point and index -- REPLACE WITH FUNCTION
-            let arrPoint = [[1, 1, 1], [1, 1, 2], [1, 1, 3]];
-            let index = 0;
-            
             // Cycle through boxes and change all colors
-            console.log(arrPoint.length);
-            for (let x = 0; x < arrPoint.length; x++) {
-                boxes[index][x].material.color.setHex('0xFF4E1F');
+            for (let x = 0; x < completed.length; x++) {
+                shipBoxes[shipIndex][x].material.color.setHex('0xFF4E1F');
             }            
-        } else if (coordinates[3] == true) {
+        } else if (hit == true) {
             // If hit
             box.material.color.setHex('0x22A113');
         } else {
@@ -112,15 +235,7 @@ $(document).ready(function() {
         scene.add(box);
         scene.add(boxLine);
     }
-
-    // Creating a couple of test boxes by passing the coordinates (X, Y, Z) into the function
-    // X, Y, Z, hit, completed
-    addBox([1, 1, 1, true, false])
-    addBox([1, 1, 2, true, false]);;
-    addBox([1, 1, 3, true, true]);
-    addBox([5, 5, 4, true, false]);
-    addBox([10, 5, 1, false, false]);
-
+    
     // Animate function
     function animate() {
         requestAnimationFrame(animate);
